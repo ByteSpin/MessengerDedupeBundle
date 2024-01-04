74fa937dd1c811629f13f9762cb87022d434304e
Greg LAMY <g.lamy@loligo.io>
2024-01-04 15:28:55 +0100

    new RemoveDedupeHashHandler for remote hash removing

---
+++ src/Model/RemoveDedupeHash.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the ByteSpin/MessengerDedupeBundle project.
+ * The project is hosted on GitHub at:
+ *  https://github.com/ByteSpin/MessengerDedupeBundle.git
+ *
+ * Copyright (c) Greg LAMY <greg@bytespin.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ByteSpin\MessengerDedupeBundle\Model;
+
+class RemoveDedupeHash
+{
+    public function __construct(
+        public string $hash,
+    ) {
+    }
+}
