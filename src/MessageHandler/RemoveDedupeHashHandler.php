37eede429da29920836e49b22b47d864946699ec
Greg LAMY <g.lamy@loligo.io>
2024-01-04 15:39:38 +0100

    code cleanup

--- src/MessageHandler/RemoveDedupeHashHandler.php
+++ src/MessageHandler/RemoveDedupeHashHandler.php
@@ -3,16 +3,10 @@
 namespace ByteSpin\MessengerDedupeBundle\MessageHandler;
 
 use AllowDynamicProperties;
-use App\Core\Manager\CoreLogManagerInterface;
-use App\Etl\Processor\Import\EtlOrderImportProcessorInterface;
 use ByteSpin\MessengerDedupeBundle\Entity\MessengerMessageHash;
-use ByteSpin\MessengerDedupeBundle\Messenger\Stamp\HashStamp;
 use ByteSpin\MessengerDedupeBundle\Model\RemoveDedupeHash;
 use ByteSpin\MessengerDedupeBundle\Repository\MessengerMessageHashRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Exception;
-use Flashmer\MessagingContracts\Sylius\OrderNew;
-use Monsieurbiz\FlashmerCore\Model\Order;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AllowDynamicProperties] #[AsMessageHandler]
